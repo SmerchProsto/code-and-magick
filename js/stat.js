82211b15c376d909f3fc20008c3957deec2c7068
Smerch <smerchprosto@gmail.com>
2019-09-08 11:37:57 +0400

    Создал окно статистики

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,14 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
 
+  ctx.fillStyle = '#ffffff';
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = '#000000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 115, 40);
+  ctx.fillText('Список результатов:', 115, 55);
 };
