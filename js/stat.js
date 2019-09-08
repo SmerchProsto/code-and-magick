f31fa1128527c297615b68a3459637e4fad23c5a
Smerch <smerchprosto@gmail.com>
2019-09-08 13:13:11 +0400

    Написал псевдокод отрисовки

--- js/stat.js
+++ js/stat.js
@@ -11,4 +11,27 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 115, 40);
   ctx.fillText('Список результатов:', 115, 55);
+
+  var WIDTH_COLUMN = 40;
+  var GAP_BETWEEN_COLUMNS = 50;
+
+  var максВысота = 150;
+
+  результатИгрока = функция(x, y, width, height, имяИгрока, времяИгрока) {
+    цикл перебора максимального числа из times, вернет максВремя;
+    имяИгрока === вы ? цвет заливки = rgba(255, 0, 0, 1) : цвет заливки = тоже синий и с рандомными каналами;
+    высотаКолонки = (150 * времяИгрока) / максВремя;
+    нарисуемПрямоугольник(x, y, WIDTH_COLUMN, высотаКолонки);
+    написатьТекст(времяИгрока x, высотаКолонки + 12);
+  }
+
+  for (var i = 0, j = 140; i < names.length; i++) {
+    ctx.fillStyle = '#000000';
+    ctx.font = '16px PT Mono';
+
+    ctx.fillText(names[i], j, 240);
+      рисуем результатИгрока(j, 225, width, height)
+
+    j += WIDTH_COLUMN + GAP_BETWEEN_COLUMNS;
+  }
 };
